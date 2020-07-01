19c55ca36f17e40bd6aeb4ed9d29ddbb7630e1d9
Mirica Yancey <myancey@fnal.gov>
2020-07-01 12:34:30 -0500

    User Login
    Addition of user login scripts. Ex. register, login, forgot password, verify email etc

---
+++ src/app/userlog/register/register.page.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import {DataService} from '../../services/data.service'
+
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  fname: string = " "
+  lname: string = " "
+  email: string = ""
+  password: string = ""
+  cpassword: string = ""
+  pin: string=""
+
+  setType(val: string){
+    this.dataService.type = val;
+  }
+  getType(): string{
+    return this.dataService.type
+  }
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    public AlertCtrl: AlertController,
+    private dataService: DataService
+
+    ) { }
+
+  ngOnInit() {
+  }
+
+ // try {
+    // const res = await this.authService.auth.createUserWithEmailAndPassword(email,password).then(
+    //     ()=>{
+    //     this.router.navigateByUrl('/tabs/tab2');
+    //   }
+    //   );
+    // } catch (error) {
+    //   console.dir(error)
+    // }
+
+  async register(): Promise<void> {
+    const { fname, lname, email, password, cpassword, pin } = this;
+    if(password !== cpassword){
+      return console.error("Passwords don't match")
+    }
+    
+    if(this.pin == '2216'){
+      this.setType("Admin")
+    }
+    else if(this.pin == '1173'){
+      this.setType("Agent")
+
+    }
+    else{
+      this.setType("Client")
+    }
+
+
+    this.authService.registerUser(email,password).then(
+      ()=>{
+      //  this.authService.sendVerificationMail();
+
+      this.authService.createUser(fname, lname, email, false, this.getType(), "assets/icon/default_icon.png", null, null, false, null)
+        this.router.navigateByUrl('/login');
+      },
+      async error => {
+        const alert = this.AlertCtrl.create({
+          message: error.message,
+          buttons: [{text: 'Ok', role:'cancel'}]
+        });
+        (await alert).present();
+      }
+    );
+  }
+}
---
+++ src/app/userlog/verify-email/verify-email.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+
+@Component({
+  selector: 'app-verify-email',
+  templateUrl: './verify-email.page.html',
+  styleUrls: ['./verify-email.page.scss'],
+})
+export class VerifyEmailPage implements OnInit {
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  sendVerificationEmail(){
+    this.authService.sendVerificationMail();
+  }
+}

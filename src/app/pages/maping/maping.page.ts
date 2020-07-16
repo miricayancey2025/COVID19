f2989ac37f35ad0d69e5ee181546afdc378071a8
Mirica Yancey <myancey@fnal.gov>
2020-07-16 02:12:02 -0500

    Map Inputed
    Map Inputed, no markers, or cards yet

--- src/app/pages/maping/maping.page.ts
+++ src/app/pages/maping/maping.page.ts
@@ -84,13 +84,6 @@ export class MapingPage implements OnInit {
         this.mapService.changePositions(this.objList, this.positions)
       })}
 
-    
-    
-    
-  
-
-
-
   pauseTimer() {
     clearInterval(this.interval);
   }
@@ -117,69 +110,32 @@ export class MapingPage implements OnInit {
     this.long =  this.loc.lng;
     this.lat = this.loc.lat;
     this.fenceList = this.firestoreService.getAllFences("valpo_fences").valueChanges()
-    this.getCount(this.fenceList)
+    //this.getCount(this.fenceList)
     this.startTimer(this.fenceList)  
      this.mapService.getObjectLastPosition("29017b68-dc6f-431c-aaaa-09e81400d956").subscribe(dat => {
          // console.log(dat);
           this.data = dat;
           this.timestamp  = this.datePipe.transform(this.data.objectState.timestamp, 'short');
      });
-
-    //  this.mapService.getFences().subscribe(fence =>{
-    //    console.log(fence)
-    //    this.fences = fence;
-    //    this.fences = Object.keys(this.fences).map(it => this.fences[it])
-    //    this.fences = this.fences[0]
-    //    this.fences.forEach(element => { 
-    //      element.populaton = 0
-    //     this.mapService.getFenceHeadCount(element.id).subscribe(data =>{
-    //       element.population = data
-    //     })
-    //    });
-    //    console.log(this.fences[0])
-
-    //  })
-     
-        //this.sum = this.mapService.getFenceHeadCount(this.fence_id)
-
-
-    // const f_db = this.db.collection("valpo_fences")
-
-    // const snapshot = await f_db.get();
-    //   snapshot.forEach(doc => {
-    //     this.mapService.getFenceHeadCount(doc.id)
-    //     //this.firestoreService.updateFence("valpo_fences",doc.id, 0)
-    //   });
-    //this.fences = this.firestoreService.getListAll("valpo_fences").valueChanges()
-
-    // this.mapService.getData(this.object_id).subscribe(data => {
-    //   console.log(data);
-    //   this.data = data;
-    //     });
-
-
-     //this.data = this.mapService.getObjectLastPosition(this.object_id).valueChanges()
-      // 41.464114, -87.040241
-  //   const cent = new tt.LngLat(-87.044285,41.462802);
-  //   const map = tt.map({
+    const cent = new tt.LngLat(-87.044285,41.462802);
+    const map = tt.map({
       
-  //     key: 'xhSLlv6eLXVggB5hbMeTK87voMmu2LV3',
+      key: 'xhSLlv6eLXVggB5hbMeTK87voMmu2LV3',
       
-  //     container: 'map',
+      container: 'map',
       
-  //     style: 'tomtom://vector/1/basic-main',
+      style: 'tomtom://vector/1/basic-main',
 
-  //     //minZoom: 10,
+      //minZoom: 10,
       
-  //     center: new tt.LngLat(-87.044285,41.462802),
+      center: new tt.LngLat(-87.044285,41.462802),
 
-  //     zoom: 15,
-
+      zoom: 14,
 
       
-  //   });
+    });
     
       
-  //   map.addControl(new tt.NavigationControl());
+    map.addControl(new tt.NavigationControl());
    }
 }
